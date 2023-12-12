856c64d5eec1dfd2bf44af86b74a9f1562bfc379
armichaud <email@alexandermichaud.tech>
2023-12-12 10:27:05 -0500

    Solved 1st part

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn expand_matrix(matrix: GalaxyMatrix) -> GalaxyMatrix {
     }
     let mut matrix = matrix;
     for row in rows_to_insert.iter().rev() {
-       matrix = matrix.insert_row(row + 1, ".".to_owned());
+       matrix = matrix.insert_rows(row + 1, 1, ".".to_owned());
     }
     let mut cols_to_insert = Vec::new();
     for (i, col) in matrix.column_iter().enumerate() {
@@ -29,18 +29,43 @@ fn expand_matrix(matrix: GalaxyMatrix) -> GalaxyMatrix {
         }
     }
     for col in cols_to_insert.iter().rev() {
-       matrix = matrix.insert_column(col + 1, ".".to_owned());
+       matrix = matrix.insert_columns(col + 1, 1, ".".to_owned());
     }
     matrix
 }
 
+fn get_galaxies(matrix: GalaxyMatrix) -> Vec<(i32, i32)> {
+    let mut galaxies = Vec::new();
+    for (i, row) in matrix.row_iter().enumerate() {
+        for (j, cell) in row.iter().enumerate() {
+            if cell == "#" {
+                galaxies.push((i as i32, j as i32));
+            }
+        }
+    }
+    galaxies
+}
+
+fn get_shortest_path(a: (i32, i32), b: (i32, i32)) -> i32 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
+}
+
 fn solution(filename: &str) -> i32 {
     let matrix = expand_matrix(get_matrix(filename));
-    println!("{}", matrix);
-    0
+    let galaxies = get_galaxies(matrix);
+    let mut sum = 0;
+    for (i, galaxy) in galaxies.iter().enumerate() {
+        for other_galaxy in galaxies.iter().skip(i + 1) {
+            sum += get_shortest_path(*galaxy, *other_galaxy);
+        }
+    }
+    sum
 }
 
 fn main() {
     assert_eq!(solution("example.txt"), 374);
-    assert_eq!(solution("input.txt"), 0); 
+    assert_eq!(solution("input.txt"), 9556896); 
+    // assert_eq!(solution("example.txt", 10), 1030);
+    // assert_eq!(solution("example.txt", 100), 8410);
+    // assert_eq!(solution("input.txt", 1000000), 0); 
 }
